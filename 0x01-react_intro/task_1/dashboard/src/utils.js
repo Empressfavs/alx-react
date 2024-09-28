a198283bea502d71029b905b3bb8f0063fc89fef
Empressfavs <favouradaavwopiabro@gmail.com>
2024-09-28 23:59:51 +0100

    Embedding expressions, functions

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,10 @@
+const getFullYear = () => new Date().getFullYear()
+
+// console.log(getFullYear());
+
+const getFooterCopy = (isIndex) => isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+
+// console.log(getFooterCopy);
+
+
+export { getFullYear, getFooterCopy }
